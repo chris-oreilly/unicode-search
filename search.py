12da7bee1c18242a041c1fa703d981a8e6273d0b
Chris O'Reilly <oreillc194@potsdam.edu>
2017-10-09 04:11:16 -0400

    Handle code point ranges

--- search.py
+++ search.py
@@ -2,6 +2,17 @@
 
 import sys
 
+def parse(line):
+    return [s.strip() for s in line.split(';', 1)]
+
+def expand(code_points, name):
+    if '..' in code_points:
+        start, end = [int(s, 16) for s in code_points.split('..', 1)]
+        for code_point in (str(s) for s in range(start, end + 1)):
+            yield code_point, name.replace('*', code_point)
+    else:
+        yield code_points, name
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('usage: {} <search>')
@@ -11,8 +22,8 @@ if __name__ == '__main__':
 
     with open('DerivedName.txt') as f:
         for line in (l.strip() for l in f):
-            if not line or line[0] == '#' or '*' in line:
+            if not line or line[0] == '#':
                 continue
-            code_point, name = [s.strip() for s in line.split(';', 1)]
-            if query.upper() in name:
-                print(chr(int(code_point, 16)), code_point, name, sep='\t')
+            for code_point, name in expand(*parse(line)):
+                if query.upper() in name:
+                    print(chr(int(code_point, 16)), code_point, name, sep='\t')
