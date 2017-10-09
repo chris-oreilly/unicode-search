a84480355b1c640fd6bd4bfec2bfe0c42b84e9e8
Chris O'Reilly <oreillc194@potsdam.edu>
2017-10-09 11:35:30 -0400

    Refactor
    Ranges are no longer expanded, but output as-is (without the characters)

--- search.py
+++ search.py
@@ -3,15 +3,14 @@
 import sys
 
 def parse(line):
-    return [s.strip() for s in line.split(';', 1)]
+    return [s.strip() for s in line.split(';', maxsplit=1)]
 
-def expand(code_points, name):
-    if '..' in code_points:
-        start, end = [int(s, 16) for s in code_points.split('..', 1)]
-        for code_point in (str(s) for s in range(start, end + 1)):
-            yield code_point, name.replace('*', code_point)
-    else:
-        yield code_points, name
+def output(code_point, name):
+    try:
+        c = chr(int(code_point, 16))
+    except ValueError:
+        c = ''
+    print(c, code_point, name, sep='\t')
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -24,6 +23,6 @@ if __name__ == '__main__':
         for line in (l.strip() for l in f):
             if not line or line[0] == '#':
                 continue
-            for code_point, name in expand(*parse(line)):
-                if query.upper() in name:
-                    print(chr(int(code_point, 16)), code_point, name, sep='\t')
+            code_point, name = parse(line)
+            if query.upper() in name:
+                output(code_point, name)
